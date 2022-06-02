88213015db323ff739ad76c57b0f3079f20f7c0d
Volodymyr Zenin <vzeni@softserveinc.com>
2022-06-02 15:17:58 +0300

    add UserSearch

--- src/props/Parent.tsx
+++ src/props/Parent.tsx
@@ -1,11 +1,12 @@
 import { Child, ChildAsFC } from './Child';
 import GuestList from '../state/GuestList';
+import UserSearch from '../state/UserSearch';
 
 const App = () => <div><h2>Hi there!</h2></div>;
 
 const Parent = () => {
     return (
-        <GuestList />
+        <UserSearch />
     );
 };
 
--- src/state/GuestList.tsx
+++ src/state/GuestList.tsx
@@ -1,6 +1,26 @@
+import { useState } from "react";
+
 const GuestList: React.FC = () => {
+    const [name, setName] = useState('');
+    const [guests, setGuests] = useState<string[]>([]);
+
+    const onClick = () => {
+        setName('');
+        setGuests([...guests, name]);
+    };
+
     return (
-        <div>Content</div>
+        <div>
+            <h3>
+                Guest List
+            </h3>
+
+            <ul>
+                {guests.map((guest, i) => <li key={guest + i}>{guest}</li>)}
+            </ul>
+            <input type="text" value={name} onChange={({ target }) => setName(target.value)}/>
+            <button onClick={onClick}>Add Guest</button>
+        </div>
     );
 }
 
